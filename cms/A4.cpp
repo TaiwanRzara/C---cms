ec1523f03c3b72eb6c4247f32c50f58f53e246db
TaiwanRzara <86676478+TaiwanRzara@users.noreply.github.com>
2023-05-27 10:27:50 +0800

    Update A4.cpp

--- cms/A4.cpp
+++ cms/A4.cpp
@@ -1,36 +1,44 @@
 #include <bits/extc++.h>
 using namespace std;
-// WA
-signed main() {
-	vector<int> graph(10000 + 5, 0);
-	int n, i;
-	cin >> n;
-	for (int s = 0, p; s < n; s++) {
-		cin >> p;
-		graph[p] = (s & 1) ? -1 : 1;
-		if (graph[p + 1] == -graph[p])
-			for (int i = p + 1; i <= 10000; i++) {
-				if (graph[i] == -graph[p]) continue;
-				else if (graph[i] == graph[p]) {
-					for (; --i > p;) graph[i] = graph[p];
-					break;
-				} else break;
-			}
-		if (graph[p - 1] == -graph[p])
-			for (int i = p - 1; i > 0; i--) {
-				if (graph[i] == -graph[p]) continue;
-				else if (graph[i] == graph[p]) {
-					for (; ++i < p;) graph[i] = graph[p];
-					break;
-				} else break;
-			}
-	}
-	i = 0;
-	for (auto v : graph) i += v;
-	if (i > 0) cout << "Repkironca";
-	else if (i < 0) cout << "BrineTw";
-	else cout << "Aaw";
+int main() {
+    int n, i, sum = 0;
+    cin >> n;
+    vector<int> graph(10005, 0);
+    for (int s = 0, p; s < n; s++) {
+        cin >> p;
+        if (s)
+            if (graph[p] != 0)
+                return 1;
+            else if (graph[p + 1] == 0 && graph[p - 1] == 0)
+                return 1;
+        graph[p] = (s & 1) ? 1 : -1;
+        // cout << "test\n";
+        for (i = p + 1; i <= 10000; i++) {
+            // cout << "t\t" << i << endl;
+            if (graph[p] == graph[i]) {
+                for (i--; i > p; i--) graph[i] = graph[p];
+                break;
+            }
+            if (graph[i] == 0) break;
+        }
+        // cout << "test\n";
+        for (i = p - 1; i > 0; i--) {
+            // cout << "t\t" << i << endl;
+            if (graph[p] == graph[i]) {
+                for (i++; i < p; i++) graph[i] = graph[p];
+                break;
+            }
+            if (graph[i] == 0) break;
+        }
+        //        for (int d = 0; d < 10005; d++)
+        //           if (graph[d])   cout << d << ". " << graph[d] << endl;
+    }
+    for (int v : graph) sum += v;
+    // cout << sum << endl;
+    if (sum > 0)
+        cout << "BrineTW\n";
+    else if (sum < 0)
+        cout << "Repkironca\n";
+    else
+        cout << "Aaw\n";
 }
-/*
-
-*/
